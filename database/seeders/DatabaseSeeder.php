8e4a563fef12edfa53760573477605cd1ba8fb33
devDevaa <112888010+devDevaa@users.noreply.github.com>
2024-08-07 17:14:25 +0630

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+use Database\Factories\ProjectFactory;
+use Database\Factories\TaskFactory;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john1111@gmail.com',
+            'password' => bcrypt('11111111'),
+            'email_verified_at' => now(),
+        ]);
+
+        Project::factory()->count(15)->hasTasks(10)->create();
+    }
+}
